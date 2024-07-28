bbd221290d4d968673510bf739528dca3157e1f0
DemsDev11 <dhemasdhiyanugraha11@gmail.com>
2024-07-28 11:48:03 +0700

    "Removed empty lines and reformatted print statements in main function."

--- tipe-data-number.go
+++ tipe-data-number.go
@@ -2,14 +2,8 @@ package main
 
 import "fmt"
 
-
 func main() {
-fmt.Println("Satu", 1)
-fmt.Println("Lima", 5)
-fmt.Println("Tiga Koma Lima", 3.5)
-
-
-
-
-
+    fmt.Println("Satu = ", 1)
+    fmt.Println("Lima = ", 5)
+    fmt.Println("Tiga Koma Lima = ", 3.5)
 }
