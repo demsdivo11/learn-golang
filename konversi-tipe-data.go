8566f5a9a2bb7a194829d6020cff5242907f8727
DemsDev11 <dhemasdhiyanugraha11@gmail.com>
2024-07-31 19:45:36 +0700

    Add konversi-tipe-data

---
+++ konversi-tipe-data.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	// konversi nilai angka (int)
+	var nilai32 int32 = 129
+var nilai64 int64 = int64(nilai32)
+var nilai8 int8 = int8(nilai32)
+fmt.Println(nilai32)
+fmt.Println(nilai64)
+fmt.Println(nila18) 
+
+
+// konversi string to byte or sebaliknya (string/str/byte/uint)
+
+var name = "Dems"
+var e byte = name[0]
+var eString string = string(e)
+fmt.Println(name)
+fmt.Println(eString)
+
+}
