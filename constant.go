09acf159c1e24b0274941fd4e96c8a45527e65e8
DemsDev11 <dhemasdhiyanugraha11@gmail.com>
2024-07-31 19:37:56 +0700

    Add isi file constant

--- constant.go
+++ constant.go
@@ -1,1 +1,14 @@
 // CONSTANT = NILAI TIDAK BISA DIUBAH
+package main
+
+import "fmt"
+
+func main() {
+	const namaPanjang = "Dhemas Dhiyanugraha"
+	const nama string = "Dems"
+	fmt.Println("CONSTANT:")
+	// tidak di panggil tidak akan error jika constant
+	// contoh constant (error)
+	//nama = "Dems Pro"
+	//namaPanjang = "Dhemas"
+}
